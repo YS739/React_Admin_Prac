f358f8aa0a81ffdf6fb2cbcdb3aa2cd9dce17fb4
YS739 <suk921@gmail.com>
2023-07-25 16:37:41 +0900

    feat:header 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
+import styles from "./Header.module.css";
+import { CgProfile } from "react-icons/cg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src="favicon.ico" alt="Logo" />
+      </div>
+      <div className={styles.title}>Admin Project</div>
+      <div className={styles.profile}>
+        <CgProfile />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
